67f66ce3dc7d82fdf3452c88364ba120353a92e6
1UnFazed1 <mahdi9779m@gmail.com>
2026-03-19 05:11:39 +0100

    Update dp8_main.py

--- dp8_main.py
+++ dp8_main.py
@@ -4,66 +4,77 @@
 import mysql.connector
 import json
 
-# ---------------- DATABASE VERBINDING ----------------
 
-verbinding = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="***",
-    database="attractiepark"
-)
+# ---------------- VERBINDING ----------------
+def maak_verbinding():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Azadegan24",
+        database="attractiepark"
+    )
 
-cursor = verbinding.cursor(dictionary=True)
 
 # ---------------- BEZOEKER OPHALEN ----------------
+def haal_bezoeker_op(cursor):
+    query = "SELECT * FROM bezoeker WHERE id = %s"
+    cursor.execute(query, (1,))
+    return cursor.fetchone()
 
-bezoeker_id = 1
-query_bezoeker = "SELECT * FROM bezoeker WHERE id = %s"
-
-cursor.execute(query_bezoeker, (bezoeker_id,))
-bezoeker = cursor.fetchone()
 
 # ---------------- ATTRACTIES OPHALEN ----------------
+def haal_attracties_op(cursor):
+    query = """
+    SELECT * FROM voorziening
+    WHERE actief = 1
+    AND (soort = 'attractie' OR soort = 'achtbaan')
+    AND geschatte_wachttijd <= 25
+    """
+    cursor.execute(query)
+    return cursor.fetchall()
 
-query_attracties = """
-SELECT * FROM voorziening
-WHERE actief = 1
-AND (soort = 'attractie' OR soort = 'achtbaan')
-AND geschatte_wachttijd <= 25
-"""
-
-cursor.execute(query_attracties)
-attracties = cursor.fetchall()
 
 # ---------------- HORECA OPHALEN ----------------
+def haal_horeca_op(cursor):
+    query = """
+    SELECT * FROM voorziening
+    WHERE actief = 1
+    AND soort = 'horeca'
+    ORDER BY geschatte_wachttijd ASC
+    LIMIT 1
+    """
+    cursor.execute(query)
+    return cursor.fetchone()
 
-query_horeca = """
-SELECT * FROM voorziening
-WHERE actief = 1
-AND soort = 'horeca'
-ORDER BY geschatte_wachttijd ASC
-LIMIT 1
-"""
 
-cursor.execute(query_horeca)
-horeca = cursor.fetchone()
+# ---------------- JSON SCHRIJVEN ----------------
+def schrijf_json(data):
+    with open("output.json", "w") as bestand:
+        json.dump(data, bestand, indent=4)
 
-# ---------------- GEGEVENS SAMENVOEGEN ----------------
 
-output_data = {
-    "bezoeker": bezoeker,
-    "geschikte_attracties": attracties,
-    "geschikte_horeca": horeca
-}
+# ---------------- MAIN PROGRAMMA ----------------
+def main():
+    verbinding = maak_verbinding()
+    cursor = verbinding.cursor(dictionary=True)
 
-# ---------------- JSON BESTAND SCHRIJVEN ----------------
+    bezoeker = haal_bezoeker_op(cursor)
+    attracties = haal_attracties_op(cursor)
+    horeca = haal_horeca_op(cursor)
 
-with open("output.json", "w") as json_bestand:
-    json.dump(output_data, json_bestand, indent=4)
+    output_data = {
+        "bezoeker": bezoeker,
+        "geschikte_attracties": attracties,
+        "geschikte_horeca": horeca
+    }
 
-# ---------------- AFSLUITEN ----------------
+    schrijf_json(output_data)
 
-print("output.json is gemaakt.")
+    print("output.json is gemaakt.")
 
-cursor.close()
-verbinding.close()
+    cursor.close()
+    verbinding.close()
+
+
+# Start het programma
+main()
